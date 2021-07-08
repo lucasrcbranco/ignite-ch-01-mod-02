40fc43849e6581dcb3c4f1a1b32f06cca0097046
lucasrbranco <lucasrcbranco@gmail.com>
2021-07-08 17:55:29 -0300

    ignite-ch-01-mod-02

---
+++ src/index.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const {v4: uuidv4} = require('uuid');
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+function verifyIfExistAccountCPF(req, res, next) {
+  const {cpf} = req.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+  if(!customer) {
+    return res.status(400).json({error: "Customer not found."});
+  }
+    req.customer = customer;
+    return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+  
+  return balance;
+}
+
+app.post('/account', (req, res) => {
+  const {name, cpf} = req.body;
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+  if(customerAlreadyExists) {
+    return res.status(400).json({error: "Customer already exists!"});
+  }
+  customers.push({
+    id: uuidv4(), 
+    name,
+    cpf,
+    statement:[]
+  });
+
+  return res.status(201).send();
+});
+
+app.get('/statement',verifyIfExistAccountCPF, (req, res) => {
+  const {customer} = req;
+  return res.json(customer.statement);
+});
+
+app.post('/deposit',verifyIfExistAccountCPF, (req,res) => {
+  const {description, amount} = req.body;
+  const {customer} = req;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistAccountCPF, (req,res) => {
+  const {amount} = req.body;
+  const {customer} = req;
+  const balance = getBalance(customer.statement);
+  if(balance < amount) {
+    return res.status(400).json({error: "Insufficient funds!"});
+  }
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+  customer.statement.push(statementOperation);
+  return res.status(201).send();
+});
+
+app.get('/statement/date',verifyIfExistAccountCPF, (req, res) => {
+  const {customer} = req;
+  const {date} = req.query;
+  const dateFormat = new Date(date + " 00:00");
+  const statement = customer.statement.filter(
+    statement => statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  );
+  return res.json(statement);
+});
+
+app.put('/account',verifyIfExistAccountCPF, (req, res) => {
+  const {name} = req.body;
+  const {customer} = req;
+  customer.name = name;
+  return res.status(201).send();
+});
+
+app.get('/account',verifyIfExistAccountCPF, (req, res) => {
+  const {customer} = req;
+
+  return res.json(customer);
+});
+
+app.delete('/account',verifyIfExistAccountCPF, (req, res) => {
+  const {customer} = req;
+  customers.splice(customer, 1);
+
+  return res.status(200).json(customers);
+})
+
+app.get('/balance',verifyIfExistAccountCPF, (req,res) => {
+  const {customer} = req;
+  const balance = getBalance(customer.statement);
+
+  return res.json(balance);
+})
+
+app.listen(3333, () => {
+  console.log(`Server is listenning at http://localhost:3333`);
+})
